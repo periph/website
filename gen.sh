679cf4e615eee266cd59919caf27dd9aff6ef94d
Marc-Antoine Ruel <maruel@chromium.org>
2017-08-02 13:37:32 -0400

    Update to marcaruel/hugo-tidy:hugo-0.25.1-alpine-3.6-pygments-2.2.0-brotli-0.6.0
    This means that brotli precompressed files will now be served. The cost is
    higher compression time (slower deployment) but the size of the content is not
    large enough to be a significant issue yet.

--- gen.sh
+++ gen.sh
@@ -7,6 +7,6 @@ set -eu
 
 cd "$(dirname $0)"
 
-TAG=marcaruel/hugo-tidy:hugo-0.25.1-alpine-3.6-pygments-2.2.0
+TAG=marcaruel/hugo-tidy:hugo-0.25.1-alpine-3.6-pygments-2.2.0-brotli-0.6.0
 docker pull $TAG
 docker run --rm -u $(id -u):$(id -g) -v $(pwd):/data $TAG
