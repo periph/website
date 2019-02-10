7d5da8e055b66af6abc6ed1d54ee6471e4930b8e
Marc-Antoine Ruel <maruel@chromium.org>
2019-02-10 17:51:17 -0500

    new_release.py: automate retrieving github username
    This was cumbersome to do this manually.
    
    Prepare code to support preparing categorization of commits.

--- new_release.py
+++ new_release.py
@@ -3,10 +3,14 @@
 # Use of this source code is governed under the Apache License, Version 2.0
 # that can be found in the LICENSE file.
 
+import collections
 import datetime
+import json
 import os
+import string
 import subprocess
 import sys
+import urllib
 
 
 TXT = """+++
@@ -70,51 +74,116 @@ updates!
 
 
 def gitlines(cmd, *args, **kwargs):
+  """Run a git commands and returns lines as a list."""
   return subprocess.check_output(['git'] + cmd, *args, **kwargs).splitlines()
 
 
-def main():
-  # TODO(maruel): Natural sorting.
-  repo = os.path.join(os.environ['GOPATH'], 'src', 'periph.io', 'x', 'periph')
+def select_range(repo):
+  master = gitlines(['rev-parse', 'origin/master'], cwd=repo)[0]
   versions = gitlines(['tag'], cwd=repo)
   version = versions[-1]
-  print('Found version %s' % version)
-  prev_version = versions[-2]
-  print('Comparing against version %s' % prev_version)
-  diff_stat = gitlines(['diff', '--stat', prev_version], cwd=repo)[-1]
-  changes = gitlines(
-      ['log', '--format=%an: %s', '%s..%s' % (prev_version, version)], cwd=repo)
-  changes.sort()
-  num_changes = len(changes)
+  version_hash = gitlines(['rev-parse', version], cwd=repo)[0]
+  if master != version_hash:
+    print('WARNING: origin/master doesn\'t match %s; diffing this' % version)
+    return master, version
 
-  # Explicitly try to handle if there's ever a ':' in one of the names.
-  authors = set(gitlines(
-      ['log', '--format=%an', '%s..%s' % (prev_version, version)], cwd=repo))
-  authors = {n:0 for n in authors}
-  # O(n^2)
-  for c in changes:
-    for n in authors:
-      if c.startswith(n+': '):
-        authors[n] += 1
-        break
+  prev_version = versions[-2]
+  print('Found version %s' % version)
+  print('Comparing against version %s' % prev_version)
+  return version, prev_version
+
+
+def email_to_user(name, email):
+  """Converts an email address to a github user name."""
+  if email.endswith('@users.noreply.github.com'):
+    return email.split('+')[1].split('@')[0]
+  url = 'https://api.github.com/search/users?q=%s+in:email' % email
+  data = json.load(urllib.urlopen(url))
+  if 'message' in data:
+    # TODO(maruel): We're getting rate limited.
+    pass
+  items = data.get('items')
+  if items:
+    login = items[0].get('login')
+    if login:
+      return login
+
+  # Try to see if the name is the user account, it is the case occasionally.
+  if set(name).issubset(string.ascii_letters + string.digits):
+    resp = urllib.urlopen('https://github.com/' + name)
+    if resp.getcode() == 200:
+      # Hit!
+      return name
+
+  print('  Failed to get GitHub user name for %s', email)
+  return email
+
+
+# TODO(maruel): For each change, determine if the change touches a single
+# package, if so, categorize it.
+Commit = collections.namedtuple(
+    'Commit', ['commit', 'author_name', 'account', 'title'])
+
+
+def get_logs(prev_version, version, repo):
+  """Get enriched logs.
+
+  Returns:
+    - list of Commit for each commit
+    - dict of name to github user account
+  """
+  cmd = ['log', '--format=%H%x00%an%x00%ae%x00%s', '%s..%s' % (prev_version, version)]
+  changes = []
+  account_mapping = {'Marc-Antoine Ruel': 'maruel'}
+  for l in gitlines(cmd, cwd=repo):
+    commit, name, email, title = l.split('\x00', 3)
+    account = None
+    if email in ('maruel@gmail.com', 'maruel@chromium.org'):
+      name = 'Marc-Antoine Ruel'
+      account = 'maruel'
+    elif email in account_mapping:
+      account = account_mapping[email]
+    else:
+      account = email_to_user(name, email)
+      account_mapping[name] = account
+    changes.append(Commit(commit, name, account, title))
+  changes.sort()
+  return changes, account_mapping
+
+
+def main():
+  repo = os.path.join(os.environ['GOPATH'], 'src', 'periph.io', 'x', 'periph')
+  version, prev_version = select_range(repo)
+  diff_stat = gitlines(['diff', '--stat', prev_version], cwd=repo)[-1]
+  changes, account_mapping = get_logs(prev_version, version, repo)
+
+  authors = {}
+  for change in changes:
+    authors.setdefault(change.author_name, []).append(change.title)
 
   now = datetime.datetime.now()
   content = TXT % {
-    'changes': '\n'.join(changes),
+    'changes': '\n'.join('%s: %s' % (c.author_name, c.title) for c in changes),
     'date': str(now.date()),
     'diff_stat': diff_stat,
     'num_authors': len(authors),
-    'num_changes': num_changes,
+    'num_changes': len(changes),
     'prev_version': prev_version,
     'thanks': '\n'.join(
-        '- %s contributed %d change%s.' % (n, c, 's' if c > 1 else '')
-        for n, c in sorted(authors.iteritems())),
+        '- [%s](https://github.com/%s) contributed %d change%s.' % (
+            author, account_mapping[author], len(titles),
+            's' if len(titles) > 1 else '')
+        for author, titles in sorted(authors.iteritems())),
     'version': version,
     'version_num': version[1:],
   }
   p = os.path.join('site', 'content', 'news', str(now.year), '%s.md' % version)
+  d = os.path.dirname(p)
+  if not os.path.isdir(d):
+    os.makedirs(d)
   with open(p, 'wb') as f:
     f.write(content)
+  print('Created %s' % p)
   return 0
 
 
