f431000e4abbced067fab854bd420743ebae2bc2
Marc-Antoine Ruel <maruel@gmail.com>
2020-12-04 20:39:26 -0500

    Announce v3.6.5 and update new_release.py
    First blog post in 2020.
    
    Make new_release.py work in python3 and make it more resilient when
    doing a lot of unauthenticated github HTTP requests.

--- new_release.py
+++ new_release.py
@@ -1,16 +1,21 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Copyright 2018 The Periph Authors. All rights reserved.
 # Use of this source code is governed under the Apache License, Version 2.0
 # that can be found in the LICENSE file.
 
+"""Generates a new periph.io release blog post."""
+
+import argparse
 import collections
 import datetime
 import json
+import logging
 import os
 import string
 import subprocess
 import sys
-import urllib
+import time
+import urllib.request
 
 
 TXT = """+++
@@ -72,10 +77,31 @@ Follow [twitter.com/periphio](https://twitter.com/periphio) for news and
 updates!
 """
 
+BACKOFF = 0
+
+def urlopen(*args, **kwargs):
+  global BACKOFF
+  if not BACKOFF:
+    BACKOFF = 1
+  else:
+    logging.debug('sleeping %gs', BACKOFF)
+    time.sleep(BACKOFF)
+    BACKOFF += 0.25
+  while True:
+    try:
+      return urllib.request.urlopen(*args, **kwargs)
+    except urllib.error.HTTPError:
+      logging.warning('Backing off more')
+      time.sleep(2*BACKOFF)
+      continue
+
 
 def gitlines(cmd, *args, **kwargs):
   """Run a git commands and returns lines as a list."""
-  return subprocess.check_output(['git'] + cmd, *args, **kwargs).splitlines()
+  cmd = ['git'] + cmd
+  logging.debug('%s', cmd)
+  return subprocess.check_output(
+      cmd, *args, **kwargs).decode('utf-8').splitlines()
 
 
 def select_range(repo):
@@ -84,12 +110,10 @@ def select_range(repo):
   version = versions[-1]
   version_hash = gitlines(['rev-parse', version], cwd=repo)[0]
   if master != version_hash:
-    print('WARNING: origin/master doesn\'t match %s; diffing this' % version)
+    logging.warning('origin/master doesn\'t match %s; diffing this', version)
     return master, version
 
   prev_version = versions[-2]
-  print('Found version %s' % version)
-  print('Comparing against version %s' % prev_version)
   return version, prev_version
 
 
@@ -97,27 +121,32 @@ def email_to_user(name, email):
   """Converts an email address to a github user name."""
   if email.endswith('@users.noreply.github.com'):
     parts = email.split('+', 2)
-    return parts[-1].split('@')[0]
+    user = parts[-1].split('@')[0]
+    logging.info('email_to_user(%s, %s) -> %s', name, email, user)
+    return user
+
   url = 'https://api.github.com/search/users?q=%s+in:email' % email
-  data = json.load(urllib.urlopen(url))
+  data = json.load(urlopen(url))
   if 'message' in data:
-    # TODO(maruel): We're getting rate limited.
-    pass
+    logging.error('getting rate limited')
   items = data.get('items')
   if items:
-    login = items[0].get('login')
-    if login:
-      return login
+    user = items[0].get('login')
+    if user:
+      logging.info('email_to_user(%s, %s) -> %s', name, email, user)
+      return user
 
   # Try to see if the name is the user account, it is the case occasionally.
   if set(name).issubset(string.ascii_letters + string.digits):
-    resp = urllib.urlopen('https://github.com/' + name)
+    time.sleep(2)
+    resp = urlopen('https://github.com/' + name)
     if resp.getcode() == 200:
       # Hit!
+      logging.info('email_to_user(%s, %s) -> %s', name, email, name)
       return name
 
-  print('  Failed to get GitHub user name for %s', email)
-  return email
+  logging.warning('email_to_user(%s, %s) Failed to get GitHub user name', name, email)
+  return None
 
 
 # TODO(maruel): For each change, determine if the change touches a single
@@ -136,14 +165,15 @@ def get_logs(prev_version, version, repo):
   cmd = ['log', '--format=%H%x00%an%x00%ae%x00%s', '%s..%s' % (prev_version, version)]
   changes = []
   account_mapping = {'Marc-Antoine Ruel': 'maruel'}
-  for l in gitlines(cmd, cwd=repo):
+  for i, l in enumerate(gitlines(cmd, cwd=repo)):
     commit, name, email, title = l.split('\x00', 3)
+    logging.debug('%d. %s %s %s', i, name, email, title)
     account = None
     if email in ('maruel@gmail.com', 'maruel@chromium.org'):
       name = 'Marc-Antoine Ruel'
       account = 'maruel'
-    elif email in account_mapping:
-      account = account_mapping[email]
+    elif name in account_mapping:
+      account = account_mapping[name]
     else:
       account = email_to_user(name, email)
       account_mapping[name] = account
@@ -152,10 +182,28 @@ def get_logs(prev_version, version, repo):
   return changes, account_mapping
 
 
+def author_thanks(author, user, count):
+  suffix = 's' if count > 1 else ''
+  if user:
+    return '- [%s](https://github.com/%s) contributed %d change%s.' % (
+        author, user, count, suffix)
+  return '- %s contributed %d change%s.' % (author, count, suffix)
+
+
 def main():
+  parser = argparse.ArgumentParser(description=sys.modules[__name__].__doc__)
+  parser.add_argument('--verbose', '-v', action='store_true')
+  parser.add_argument('--from', dest='from_version', help='Override the base revision, e.g. v3.6.2')
+  args = parser.parse_args()
+  logging.basicConfig(level=logging.DEBUG if args.verbose else logging.WARNING)
+
   gopath = os.environ.get('GOPATH') or os.path.expanduser(os.path.join('~', 'go'))
   repo = os.path.join(gopath, 'src', 'periph.io', 'x', 'periph')
   version, prev_version = select_range(repo)
+  if args.from_version:
+    prev_version = args.from_version
+  print('Found version %s' % version)
+  print('Comparing against version %s' % prev_version)
   diff_stat = gitlines(['diff', '--stat', prev_version], cwd=repo)[-1]
   changes, account_mapping = get_logs(prev_version, version, repo)
 
@@ -172,10 +220,8 @@ def main():
     'num_changes': len(changes),
     'prev_version': prev_version,
     'thanks': '\n'.join(
-        '- [%s](https://github.com/%s) contributed %d change%s.' % (
-            author, account_mapping[author], len(titles),
-            's' if len(titles) > 1 else '')
-        for author, titles in sorted(authors.iteritems())),
+        author_thanks(author, account_mapping[author], len(titles))
+        for author, titles in sorted(authors.items())),
     'version': version,
     'version_num': version[1:],
   }
@@ -183,7 +229,7 @@ def main():
   d = os.path.dirname(p)
   if not os.path.isdir(d):
     os.makedirs(d)
-  with open(p, 'wb') as f:
+  with open(p, 'w') as f:
     f.write(content)
   print('Created %s' % p)
   return 0
